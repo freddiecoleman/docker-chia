aaed196d086d15941b9c47bff3cbef9e363f2e08
Freddie Coleman <f.coleman@hotmail.co.uk>
2020-07-11 14:41:45 +0100

    try latest dev branch to get ready for next release

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ cd /chia-blockchain
 . ./activate
 chia init
 chia keys generate
-sed -i 's/localhost/chia/g' /.chia/beta-1.0b8.dev55/config/config.yaml \
+sed -i 's/localhost/chia/g' /.chia/beta-1.0b8.dev192/config/config.yaml \
 chia start node -r
 tail -f /dev/null
